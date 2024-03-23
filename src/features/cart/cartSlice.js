f00beb6bcfca87e90b3a92280166ddbd8f875869
PratikMane09 <Pratik09092001@gmail.com>
2024-03-23 23:43:47 +0530

    cart operation

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -32,8 +32,10 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => (item.pizzaId = action.payload));
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
-    clearCart(state, action) {
+    clearCart(state) {
       state.cart = [];
     },
   },
@@ -48,3 +50,13 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityByID = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
